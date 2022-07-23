dfd701febce262ccfd9671316f13b283f42fd460
= <abhidnya09@gmail.com>
2022-07-23 21:17:00 +0530

    Revert "The Recycler View Is Ready"
    This reverts commit 1ee1d7b7767315e2b6465f1c642df25318bc15a1.

--- app/src/main/java/com/example/recyclerview/FriendsAdapter.java
+++ app/src/main/java/com/example/recyclerview/FriendsAdapter.java
@@ -1,45 +1,13 @@
 package com.example.recyclerview;
 
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-
-public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendViewHolder> {
-    Context context;
-    ArrayList<Friends> friends;
-
-    public FriendsAdapter(Context context, ArrayList<Friends> friends) {
-        this.context = context;
-        this.friends = friends;
-    }
-
-    @NonNull
-    @Override
-    public FriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.sample_row,parent,false);
-        return new FriendViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull FriendViewHolder holder, int position) {
-    Friends friend = friends.get(position);
-    holder.friendimage.setImageResource(friend.getImage());
-    holder.titles.setText(friend.getTitle());
-    }
-
-    @Override
-    public int getItemCount() {
-        return friends.size();
-    }
-
+public class FriendsAdapter {
     public class FriendViewHolder extends RecyclerView.ViewHolder {
         ImageView friendimage;
         TextView titles;
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -15,17 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.recyclerview);
-        ArrayList<Friends> friends = new ArrayList<>();
-        friends.add(new Friends("Aditya",R.drawable.aditya));
-        friends.add(new Friends("Jyotiram",R.drawable.jyotiram));
-        friends.add(new Friends("Atharva",R.drawable.atharva));
-        friends.add(new Friends("Maharaj",R.drawable.maharaj));
-        FriendsAdapter friendsAdapter = new FriendsAdapter(this,friends);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(friendsAdapter);
-
-
-
     }
 }
