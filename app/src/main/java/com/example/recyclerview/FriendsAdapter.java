bd3805f46acc17ae05d31bb52d2f190c3cd37ab2
= <abhidnya09@gmail.com>
2022-07-24 10:40:20 +0530

    recyclerView Grid Are Add

--- app/src/main/java/com/example/recyclerview/FriendsAdapter.java
+++ app/src/main/java/com/example/recyclerview/FriendsAdapter.java
@@ -1,13 +1,45 @@
 package com.example.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FriendsAdapter {
+import java.util.ArrayList;
+
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendViewHolder> {
+    Context context;
+    ArrayList<Friends> friend;
+
+    public FriendsAdapter(Context context, ArrayList<Friends> friend) {
+        this.context = context;
+        this.friend = friend;
+    }
+
+    @NonNull
+    @Override
+    public FriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_row,parent,false);
+        return new FriendViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendViewHolder holder, int position) {
+        Friends friends = friend.get(position);
+        holder.friendimage.setImageResource(friends.getImage());
+        holder.titles.setText(friends.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return friend.size();
+    }
+
     public class FriendViewHolder extends RecyclerView.ViewHolder {
         ImageView friendimage;
         TextView titles;
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,16 +1,53 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    Button grid , linear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         recyclerView = findViewById(R.id.recyclerview);
+        grid = findViewById(R.id.grid);
+        linear = findViewById(R.id.linear);
+        ArrayList<Friends> friends = new ArrayList<>();
+        friends.add(new Friends("Aditya",R.drawable.aditya));
+        friends.add(new Friends("Jyotiram",R.drawable.jyotiram));
+        friends.add(new Friends("Atharva",R.drawable.atharva));
+        friends.add(new Friends("Maharaj",R.drawable.maharaj));
+        FriendsAdapter ad = new FriendsAdapter(getApplicationContext(),friends);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(gridLayoutManager);
+            }
+        });
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recyclerView.setLayoutManager(linearLayoutManager);
+            }
+        });
+
+        recyclerView.setAdapter(ad);
+
+
+
     }
 }
